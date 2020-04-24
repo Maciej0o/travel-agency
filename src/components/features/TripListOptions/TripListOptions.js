921cafb8cb692b4f0aa6111fc4a2203fe3400e25
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-04-24 21:52:40 +0200

    Add filters Trip List

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -9,15 +9,18 @@ class TripListOptions extends React.Component {
     if(checked) {
       console.log('Adding tag', tag);
       // TODO - use action dispatcher from props
+      this.props.addTripTag({ tag });
     } else {
       console.log('Removing tag', tag);
       // TODO - use action dispatcher from props
+      this.props.removeTripTag({ tag });
     }
   }
 
   handleDuration(type, value){
     console.log('Changing duration', type, value);
     // TODO - use action dispatcher from props
+    this.props.changeTripDuration({ type, value });
   }
 
   handleSearch(phrase){
@@ -73,6 +76,9 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  changeTripDuration: PropTypes.func,
+  addTripTag: PropTypes.func,
+  removeTripTag: PropTypes.func,
 };
 
 export default TripListOptions;
--- src/redux/filtersRedux.js
+++ src/redux/filtersRedux.js
@@ -11,10 +11,18 @@ const createActionName = name => `app/${reducerName}/${name}`;
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
 // TODO - add other action types
+export const CHANGE_DURATION = createActionName('CHANGE_DURATION');
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
+export const changeTripDuration = payload => ({ payload, type: CHANGE_DURATION });
+export const addTripTag = payload => ({ payload, type: ADD_TAG });
+export const removeTripTag = payload => ({ payload, type: REMOVE_TAG });
+
 // TODO - add other action creators
+
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -23,6 +31,29 @@ export default function reducer(statePart = [], action = {}) {
       return {
         ...statePart,
         searchPhrase: action.payload,
+      };
+    case CHANGE_DURATION:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          [action.payload.type]: action.payload.value,
+        },
+      };
+    case ADD_TAG:
+      return {
+        ...statePart,
+        tags: [
+          ...statePart.tags,
+          action.payload.tag,
+        ],
+      };
+    case REMOVE_TAG:
+      return {
+        ...statePart,
+        tags: [
+          ...statePart.tags.filter(tag => tag != action.payload.tag),
+        ],
       };
     // TODO - handle other action types
     default:
