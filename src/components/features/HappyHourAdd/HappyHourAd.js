b50c8402060ec9930b9ae399ca9a33482f6d914b
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-06-08 20:40:28 +0200

    Add HappyHourAd and make test 1 and 2

---
+++ src/components/features/HappyHourAdd/HappyHourAd.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './HappyHourAd.scss';
+
+class HappyHourAd extends React.Component {
+
+  render() {
+    return(
+      <div>
+        <h3 className={styles.title}></h3>
+        <div className={styles.promoDescription}></div>
+      </div>
+    );
+  }
+}
+
+export default HappyHourAd;
