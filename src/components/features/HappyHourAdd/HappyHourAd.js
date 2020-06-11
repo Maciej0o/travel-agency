87abbe2253c6b9932f5e5338b14b6cbd9a92ba86
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-06-11 16:56:55 +0200

    Add HappzHourAd to Hero.js

--- src/components/features/HappyHourAdd/HappyHourAd.js
+++ src/components/features/HappyHourAdd/HappyHourAd.js
@@ -40,7 +40,7 @@ class HappyHourAd extends React.Component {
     }  
 
     return(
-      <div>
+      <div className={styles.component}>
         <h3 className={styles.title}>{title}</h3>
         <div className={styles.promoDescription}>{announcement}</div>
       </div>
